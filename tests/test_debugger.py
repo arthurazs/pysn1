6473945a66df7645017d6fbcfed972875cdbf852
Arthur Zopellaro <github@zope.me>
2024-04-09 14:07:21 -0300

    ref: pep8

--- tests/test_debugger.py
+++ tests/test_debugger.py
@@ -1,4 +1,4 @@
-from pysn1.debugger import Identifier, IdentifierClass, PrimitiveType, ContextType, ApplicationType
+from pysn1.debugger import ApplicationType, ContextType, Identifier, IdentifierClass, PrimitiveType
 
 
 class TestDebugger:
@@ -9,7 +9,11 @@ class TestDebugger:
         pass
 
     def test_identifier_universal(self: "TestDebugger") -> None:
-        identifier = Identifier(reference=IdentifierClass.UNIVERSAL, constructed=False, datatype=PrimitiveType.END_OF_CONTENT)
+        identifier = Identifier(
+            reference=IdentifierClass.UNIVERSAL,
+            constructed=False,
+            datatype=PrimitiveType.END_OF_CONTENT,
+        )
         assert identifier.reference == IdentifierClass.UNIVERSAL
         assert identifier.primitive is True
         assert identifier.constructed is False
@@ -18,12 +22,16 @@ class TestDebugger:
         assert bytes(identifier) == b"\x00"
 
     def test_identifier_application(self: "TestDebugger") -> None:
-        identifier = Identifier(reference=IdentifierClass.APPLICATION, constructed=False, datatype=ApplicationType(value=0))
+        identifier = Identifier(
+            reference=IdentifierClass.APPLICATION,
+            constructed=False,
+            datatype=ApplicationType(value=0),
+        )
         assert identifier.reference == IdentifierClass.APPLICATION
         assert identifier.primitive is True
         assert identifier.constructed is False
         assert identifier.datatype == ApplicationType(value=0)
-        assert int(identifier) == 0x40
+        assert int(identifier) == 0x40  # noqa: PLR2004
         assert bytes(identifier) == b"\x40"
 
     def test_identifier_context(self: "TestDebugger") -> None:
@@ -32,25 +40,33 @@ class TestDebugger:
         assert identifier.primitive is True
         assert identifier.constructed is False
         assert identifier.datatype == ContextType(value=0)
-        assert int(identifier) == 0x80
+        assert int(identifier) == 0x80  # noqa: PLR2004
         assert bytes(identifier) == b"\x80"
 
     def test_identifier_private(self: "TestDebugger") -> None:
-        identifier = Identifier(reference=IdentifierClass.PRIVATE, constructed=False, datatype=PrimitiveType.END_OF_CONTENT)
+        identifier = Identifier(
+            reference=IdentifierClass.PRIVATE,
+            constructed=False,
+            datatype=PrimitiveType.END_OF_CONTENT,
+        )
         assert identifier.reference == IdentifierClass.PRIVATE
         assert identifier.primitive is True
         assert identifier.constructed is False
         assert identifier.datatype == PrimitiveType.END_OF_CONTENT
-        assert int(identifier) == 0xC0
+        assert int(identifier) == 0xC0  # noqa: PLR2004
         assert bytes(identifier) == b"\xC0"
 
     def test_identifier_constructed(self: "TestDebugger") -> None:
-        identifier = Identifier(reference=IdentifierClass.UNIVERSAL, constructed=True, datatype=PrimitiveType.END_OF_CONTENT)
+        identifier = Identifier(
+            reference=IdentifierClass.UNIVERSAL,
+            constructed=True,
+            datatype=PrimitiveType.END_OF_CONTENT,
+        )
         assert identifier.reference == IdentifierClass.UNIVERSAL
         assert identifier.primitive is False
         assert identifier.constructed is True
         assert identifier.datatype == PrimitiveType.END_OF_CONTENT
-        assert int(identifier) == 0x20
+        assert int(identifier) == 0x20  # noqa: PLR2004
         assert bytes(identifier) == b"\x20"
 
     def test_identifier_boolean(self: "TestDebugger") -> None:
@@ -63,12 +79,16 @@ class TestDebugger:
         assert bytes(identifier) == b"\x01"
 
     def test_identifier_application_value(self: "TestDebugger") -> None:
-        identifier = Identifier(reference=IdentifierClass.APPLICATION, constructed=False, datatype=ApplicationType(value=1))
+        identifier = Identifier(
+            reference=IdentifierClass.APPLICATION,
+            constructed=False,
+            datatype=ApplicationType(value=1),
+        )
         assert identifier.reference == IdentifierClass.APPLICATION
         assert identifier.primitive is True
         assert identifier.constructed is False
         assert identifier.datatype == ApplicationType(value=1)
-        assert int(identifier) == 0x41
+        assert int(identifier) == 0x41  # noqa: PLR2004
         assert bytes(identifier) == b"\x41"
 
     def test_identifier_context_value(self: "TestDebugger") -> None:
@@ -77,7 +97,7 @@ class TestDebugger:
         assert identifier.primitive is True
         assert identifier.constructed is False
         assert identifier.datatype == ContextType(value=1)
-        assert int(identifier) == 0x81
+        assert int(identifier) == 0x81  # noqa: PLR2004
         assert bytes(identifier) == b"\x81"
 
 
@@ -97,7 +117,7 @@ class TestDebuggerFromInt:
         assert identifier.primitive is True
         assert identifier.constructed is False
         assert identifier.datatype == ApplicationType(value=0)
-        assert int(identifier) == 0x40
+        assert int(identifier) == 0x40  # noqa: PLR2004
         assert bytes(identifier) == b"\x40"
 
     def test_from_int_context(self: "TestDebuggerFromInt") -> None:
@@ -106,7 +126,7 @@ class TestDebuggerFromInt:
         assert identifier.primitive is True
         assert identifier.constructed is False
         assert identifier.datatype == ContextType(value=0)
-        assert int(identifier) == 0x80
+        assert int(identifier) == 0x80  # noqa: PLR2004
         assert bytes(identifier) == b"\x80"
 
     def test_from_int_private(self: "TestDebuggerFromInt") -> None:
@@ -115,7 +135,7 @@ class TestDebuggerFromInt:
         assert identifier.primitive is True
         assert identifier.constructed is False
         assert identifier.datatype == PrimitiveType.END_OF_CONTENT
-        assert int(identifier) == 0xC0
+        assert int(identifier) == 0xC0  # noqa: PLR2004
         assert bytes(identifier) == b"\xC0"
 
     def test_from_int_constructed(self: "TestDebuggerFromInt") -> None:
@@ -124,7 +144,7 @@ class TestDebuggerFromInt:
         assert identifier.primitive is False
         assert identifier.constructed is True
         assert identifier.datatype == PrimitiveType.END_OF_CONTENT
-        assert int(identifier) == 0x20
+        assert int(identifier) == 0x20  # noqa: PLR2004
         assert bytes(identifier) == b"\x20"
 
     def test_from_int_boolean(self: "TestDebuggerFromInt") -> None:
@@ -142,7 +162,7 @@ class TestDebuggerFromInt:
         assert identifier.primitive is True
         assert identifier.constructed is False
         assert identifier.datatype == ApplicationType(value=1)
-        assert int(identifier) == 0x41
+        assert int(identifier) == 0x41  # noqa: PLR2004
         assert bytes(identifier) == b"\x41"
 
     def test_from_int_context_value(self: "TestDebuggerFromInt") -> None:
@@ -151,7 +171,7 @@ class TestDebuggerFromInt:
         assert identifier.primitive is True
         assert identifier.constructed is False
         assert identifier.datatype == ContextType(value=1)
-        assert int(identifier) == 0x81
+        assert int(identifier) == 0x81  # noqa: PLR2004
         assert bytes(identifier) == b"\x81"
 
 
@@ -171,7 +191,7 @@ class TestDebuggerFromBytes:
         assert identifier.primitive is True
         assert identifier.constructed is False
         assert identifier.datatype == ApplicationType(value=0)
-        assert int(identifier) == 0x40
+        assert int(identifier) == 0x40  # noqa: PLR2004
         assert bytes(identifier) == b"\x40"
 
     def test_from_int_context(self: "TestDebuggerFromBytes") -> None:
@@ -180,7 +200,7 @@ class TestDebuggerFromBytes:
         assert identifier.primitive is True
         assert identifier.constructed is False
         assert identifier.datatype == ContextType(value=0)
-        assert int(identifier) == 0x80
+        assert int(identifier) == 0x80  # noqa: PLR2004
         assert bytes(identifier) == b"\x80"
 
     def test_from_int_private(self: "TestDebuggerFromBytes") -> None:
@@ -189,7 +209,7 @@ class TestDebuggerFromBytes:
         assert identifier.primitive is True
         assert identifier.constructed is False
         assert identifier.datatype == PrimitiveType.END_OF_CONTENT
-        assert int(identifier) == 0xC0
+        assert int(identifier) == 0xC0  # noqa: PLR2004
         assert bytes(identifier) == b"\xC0"
 
     def test_from_int_constructed(self: "TestDebuggerFromBytes") -> None:
@@ -198,7 +218,7 @@ class TestDebuggerFromBytes:
         assert identifier.primitive is False
         assert identifier.constructed is True
         assert identifier.datatype == PrimitiveType.END_OF_CONTENT
-        assert int(identifier) == 0x20
+        assert int(identifier) == 0x20  # noqa: PLR2004
         assert bytes(identifier) == b"\x20"
 
     def test_from_int_boolean(self: "TestDebuggerFromBytes") -> None:
@@ -216,7 +236,7 @@ class TestDebuggerFromBytes:
         assert identifier.primitive is True
         assert identifier.constructed is False
         assert identifier.datatype == ApplicationType(value=1)
-        assert int(identifier) == 0x41
+        assert int(identifier) == 0x41  # noqa: PLR2004
         assert bytes(identifier) == b"\x41"
 
     def test_from_int_context_value(self: "TestDebuggerFromBytes") -> None:
@@ -225,5 +245,5 @@ class TestDebuggerFromBytes:
         assert identifier.primitive is True
         assert identifier.constructed is False
         assert identifier.datatype == ContextType(value=1)
-        assert int(identifier) == 0x81
+        assert int(identifier) == 0x81  # noqa: PLR2004
         assert bytes(identifier) == b"\x81"
--- tests/test_triplet.py
+++ tests/test_triplet.py
@@ -19,66 +19,69 @@ class TestTripletFromBytes:
     def test_from_bytes(self: "TestTripletFromBytes") -> None:
         bytestring = b"\x81\x01\x01"
         triplet = t.Triplet.from_bytes(bytestring)
-        assert triplet.tag == 0x81
+        assert triplet.tag == DEFAULT_TAG
         assert triplet.length == 1
         assert triplet.value == b"\x01"
-        assert len(triplet) == 3
+        assert len(triplet) == len(bytestring)
         assert bytes(triplet) == bytestring
 
     def test_from_bytes_empty_value(self: "TestTripletFromBytes") -> None:
         bytestring = b"\x81\x00"
         triplet = t.Triplet.from_bytes(bytestring)
-        assert triplet.tag == 0x81
+        assert triplet.tag == DEFAULT_TAG
         assert triplet.length == 0
         assert triplet.value == b""
-        assert len(triplet) == 2
+        assert len(triplet) == len(bytestring)
         assert bytes(triplet) == bytestring
 
     def test_from_bytes_no_extended_length(self: "TestTripletFromBytes") -> None:
-        value = (b"\x00" * 0x7E) + b"\x01"
+        length = 0x7F
+        value = (b"\x00" * (length - 1)) + b"\x01"
         bytestring = b"\x81\x7F" + value
         triplet = t.Triplet.from_bytes(bytestring)
-        assert triplet.tag == 0x81
-        assert triplet.length == 0x7F
+        assert triplet.tag == DEFAULT_TAG
+        assert triplet.length == length
         assert triplet.value == value
-        assert len(triplet) == 0x7F + 2
+        assert len(triplet) == len(bytestring)
         assert bytes(triplet) == bytestring
 
     def test_from_bytes_extended_length_1(self: "TestTripletFromBytes") -> None:
-        value = (b"\x00" * 0x7F) + b"\x01"
+        length = 0x80
+        value = (b"\x00" * (length - 1)) + b"\x01"
         bytestring = b"\x81\x81\x80" + value
         triplet = t.Triplet.from_bytes(bytestring)
-        assert triplet.tag == 0x81
-        assert triplet.length == 0x80
+        assert triplet.tag == DEFAULT_TAG
+        assert triplet.length == length
         assert triplet.value == value
-        assert len(triplet) == 0x80 + 3
+        assert len(triplet) == len(bytestring)
         assert bytes(triplet) == bytestring
 
     def test_from_bytes_extended_length_2(self: "TestTripletFromBytes") -> None:
-        value = (b"\x00" * 0xFF) + b"\x01"
+        length = 0x100
+        value = (b"\x00" * (length - 1)) + b"\x01"
         bytestring = b"\x81\x82\x01\x00" + value
         triplet = t.Triplet.from_bytes(bytestring)
-        assert triplet.tag == 0x81
-        assert triplet.length == 0xFF + 1
+        assert triplet.tag == DEFAULT_TAG
+        assert triplet.length == length
         assert triplet.value == value
-        assert len(triplet) == 0xFF + 1 + 4
+        assert len(triplet) == len(bytestring)
         assert bytes(triplet) == bytestring
 
     def test_from_bytes_too_many_bytes(self: "TestTripletFromBytes") -> None:
         value = b"\x00"
         bytestring = b"\x81\x01" + value + b"\x01"
         triplet = t.Triplet.from_bytes(bytestring)
-        assert triplet.tag == 0x81
+        assert triplet.tag == DEFAULT_TAG
         assert triplet.length == 1
         assert triplet.value == value
-        assert len(triplet) == 3
+        assert len(triplet) == len(bytestring)
         assert bytes(triplet) == bytestring[:-1]
 
     @pytest.mark.skip(reason="too slow")
     def test_from_bytes_too_big(self: "TestTripletFromBytes") -> None:
+        value = (b"\x00" * t.EXTENDED_LENGTH_4) + b"\x01"
+        bytestring = b"\x81\x85\x01\x00\x00\x00\x00" + value
         with pytest.raises(t.TripletLengthTooBigError) as exc_info:
-            value = (b"\x00" * t.EXTENDED_LENGTH_4) + b"\x01"
-            bytestring = b"\x81\x85\x01\x00\x00\x00\x00" + value
             t.Triplet.from_bytes(bytestring)
         assert exc_info.match("Triplet with extended length > 4 not implemented")
 
@@ -106,75 +109,78 @@ class TestTripletFromBytes:
 class TestTriplet:
     def test_from_bytes(self: "TestTriplet") -> None:
         bytestring = b"\x81\x01\x01"
-        triplet = t.Triplet(tag=0x81, length=1, value=b"\x01")
-        assert triplet.tag == 0x81
+        triplet = t.Triplet(tag=DEFAULT_TAG, length=1, value=b"\x01")
+        assert triplet.tag == DEFAULT_TAG
         assert triplet.length == 1
         assert triplet.value == b"\x01"
-        assert len(triplet) == 3
+        assert len(triplet) == len(bytestring)
         assert bytes(triplet) == bytestring
 
     def test_from_bytes_empty_value(self: "TestTriplet") -> None:
         bytestring = b"\x81\x00"
-        triplet = t.Triplet(tag=0x81, length=0)
-        assert triplet.tag == 0x81
+        triplet = t.Triplet(tag=DEFAULT_TAG, length=0)
+        assert triplet.tag == DEFAULT_TAG
         assert triplet.length == 0
         assert triplet.value == b""
-        assert len(triplet) == 2
+        assert len(triplet) == len(bytestring)
         assert bytes(triplet) == bytestring
 
     def test_from_bytes_no_extended_length(self: "TestTriplet") -> None:
-        value = (b"\x00" * 0x7E) + b"\x01"
+        length = 0x7F
+        value = (b"\x00" * (length - 1)) + b"\x01"
         bytestring = b"\x81\x7F" + value
-        triplet = t.Triplet(tag=0x81, length=0x7F, value=value)
-        assert triplet.tag == 0x81
-        assert triplet.length == 0x7F
+        triplet = t.Triplet(tag=DEFAULT_TAG, length=length, value=value)
+        assert triplet.tag == DEFAULT_TAG
+        assert triplet.length == length
         assert triplet.value == value
-        assert len(triplet) == 0x7F + 2
+        assert len(triplet) == len(bytestring)
         assert bytes(triplet) == bytestring
 
     def test_from_bytes_extended_length_1(self: "TestTriplet") -> None:
-        value = (b"\x00" * 0x7F) + b"\x01"
+        length = 0x80
+        value = (b"\x00" * (length - 1)) + b"\x01"
         bytestring = b"\x81\x81\x80" + value
-        triplet = t.Triplet(tag=0x81, length=0x80, value=value)
-        assert triplet.tag == 0x81
-        assert triplet.length == 0x80
+        triplet = t.Triplet(tag=DEFAULT_TAG, length=length, value=value)
+        assert triplet.tag == DEFAULT_TAG
+        assert triplet.length == length
         assert triplet.value == value
-        assert len(triplet) == 0x80 + 3
+        assert len(triplet) == len(bytestring)
         assert bytes(triplet) == bytestring
 
     def test_from_bytes_extended_length_2(self: "TestTriplet") -> None:
-        value = (b"\x00" * 0xFF) + b"\x01"
+        length = 0x100
+        value = (b"\x00" * (length - 1)) + b"\x01"
         bytestring = b"\x81\x82\x01\x00" + value
-        triplet = t.Triplet(tag=0x81, length=0xFF + 1, value=value)
-        assert triplet.tag == 0x81
-        assert triplet.length == 0xFF + 1
+        triplet = t.Triplet(tag=DEFAULT_TAG, length=length, value=value)
+        assert triplet.tag == DEFAULT_TAG
+        assert triplet.length == length
         assert triplet.value == value
-        assert len(triplet) == 0xFF + 1 + 4
+        assert len(triplet) == len(bytestring)
         assert bytes(triplet) == bytestring
 
     @pytest.mark.skip(reason="too slow")
     def test_from_bytes_too_big_correct_value(self: "TestTriplet") -> None:
+        value = (b"\x00" * t.EXTENDED_LENGTH_4) + b"\x01"
         with pytest.raises(t.TripletLengthTooBigError) as exc_info:
-            value = (b"\x00" * t.EXTENDED_LENGTH_4) + b"\x01"
-            t.Triplet(tag=0x81, length=0xFFFFFFFF + 1, value=value)
+            t.Triplet(tag=DEFAULT_TAG, length=0xFFFFFFFF + 1, value=value)
         assert exc_info.match("Triplet with extended length > 4 not implemented")
 
     def test_from_bytes_too_big_missing_value(self: "TestTriplet") -> None:
         with pytest.raises(t.TripletLengthTooBigError) as exc_info:
-            t.Triplet(tag=0x81, length=0xFFFFFFFF + 1, value=b"")
+            t.Triplet(tag=DEFAULT_TAG, length=0xFFFFFFFF + 1, value=b"")
         assert exc_info.match("Triplet with extended length > 4 not implemented")
 
     def test_from_bytes_too_many_bytes(self: "TestTriplet") -> None:
         with pytest.raises(t.TripletTooManyBytesError) as exc_info:
-            t.Triplet(tag=0x81, length=1, value=b"\x00\x01")
+            t.Triplet(tag=DEFAULT_TAG, length=1, value=b"\x00\x01")
         assert exc_info.match("Triplet length is 1, but value contains 2 bytes")
 
     def test_from_bytes_missing_bytes_none(self: "TestTriplet") -> None:
         with pytest.raises(t.TripletMissingBytesError) as exc_info:
-            t.Triplet(tag=0x81, length=1)
+            t.Triplet(tag=DEFAULT_TAG, length=1)
         assert exc_info.match("Triplet length is 1, but value contains only 0 bytes")
 
     def test_from_bytes_missing_bytes_value(self: "TestTriplet") -> None:
         with pytest.raises(t.TripletMissingBytesError) as exc_info:
-            t.Triplet(tag=0x81, length=2, value=b"\x01")
+            t.Triplet(tag=DEFAULT_TAG, length=2, value=b"\x01")
         assert exc_info.match("Triplet length is 2, but value contains only 1 bytes")
